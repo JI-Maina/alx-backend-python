092f1da5db0d09c43c27dc71d9a1203555eccb8d
JI-Maina <irungujmaina@gmail.com>
2023-07-11 09:18:45 +0000

    modifeid random.uniform parameters

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,6 +11,6 @@ async def async_generator():
     """yields a random number between 0 and 10"""
 
     for _ in range(10):
-        x = random.uniform(0, 10)
+        x = random.uniform(1, 9)
         await asyncio.sleep(1)
         yield x
